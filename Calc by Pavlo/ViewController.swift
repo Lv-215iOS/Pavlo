6ab49158d24374f796f619edeb29a45873cb2900
pasik_01 <pavlotcubylski@gmail.com>
2016-12-07 10:59:15 +0200

    Initial Commit

---
+++ Calc by Pavlo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calc by Pavlo
+//
+//  Created by pasik_01 on 07.12.16.
+//  Copyright © 2016 pasik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
